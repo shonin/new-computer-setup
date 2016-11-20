884b1418dc78f9b9b0d4486082df8ce8dd4e63ec
Elliot Charney <elliot.charney@ithaka.org>
2016-11-20 12:19:59 -0500

    alter

--- git_alias.sh
+++ git_alias.sh
@@ -8,6 +8,11 @@ create_aliases() {
   git config --global alias.co checkout
   git config --global alias.cm 'commit -m'
   git config --global alias.cam 'commit -am'
+  git config --global alias.aa 'add -A'
+}
+
+create_better_defaults() {
+  git config --global push.default current
 }
 
 setup_user
