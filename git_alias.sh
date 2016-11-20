c682e8b2dbddd83495d967b14a05145de475de90
Elliot Charney <elliot.charney@ithaka.org>
2016-11-20 11:55:42 -0500

    init

---
+++ git_alias.sh
@@ -0,0 +1,7 @@
+
+create_aliases() {
+  git config --global alias.s status
+  git config --global alias.co checkout
+}
+
+create_aliases
---
+++ ssh_setup.sh
@@ -0,0 +1,41 @@
+readonly SSH_DIR=${HOME}/.ssh
+readonly SSH_KEY=${SSH_DIR}/imac
+readonly EMAIL=elliot.charney@ithaka.org
+
+generate_ssh_config() {
+    cat <<CONFIG
+Host *
+    IdentitiesOnly yes
+Host github.com
+    User git
+    IdentityFile $SSH_KEY
+Host github-shared-libs
+    User git
+    Hostname github.com
+    IdentityFile $SSH_KEY
+    StrictHostKeyChecking no
+CONFIG
+}
+
+configure_ssh() {
+    mkdir -p $SSH_DIR
+
+    if [ ! -f $SSH_KEY ]; then
+        ssh-keygen -t rsa -b 4096 -C \"$EMAIL\" -f $SSH_KEY
+        chmod 400 $SSH_KEY
+    fi
+
+    if [ -z "$SSH_AUTH_SOCK" ] ; then
+        eval "$(ssh-agent -s)"
+    fi
+    ssh-add ${SSH_KEY}
+    pbcopy < ${SSH_KEY}.pub
+
+    echo Your public SSH key has been copied to your clipboard for you.
+    echo Go to https://github.com/settings/keys to add it to your GitHub account.
+    read -p "Press enter when this has been done: " entered
+
+    generate_ssh_config >> $SSH_DIR/config
+}
+
+configure_ssh
