c293c47b6b64586559831a1ca458cdc808e10993
Elliot Charney <elliot.charney@ithaka.org>
2016-11-20 11:56:53 -0500

    add user

--- git_alias.sh
+++ git_alias.sh
@@ -1,7 +1,13 @@
+setup_user() {
+  git config --global user.name "Elliot Charney"
+  git config --global user.email elliot.charney@ithaka.org
+}
 
 create_aliases() {
   git config --global alias.s status
   git config --global alias.co checkout
+  git config --global alias.cm commit
 }
 
+setup_user
 create_aliases
